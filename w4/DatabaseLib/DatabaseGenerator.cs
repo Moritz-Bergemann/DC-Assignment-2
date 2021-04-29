a6b22dab2623aee2003b2c5b060868e3ab2822be
Moritz Bergemann <moritz.bergemann@gmail.com>
2021-04-29 19:13:16 +0800

    feat: Add workshop 4

---
+++ w4/DatabaseLib/DatabaseGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLib
+{
+    internal class DatabaseGenerator
+    {
+        private static string[] firstNames = { "Ethyl", "Rebekah", "Milissa", "Liberty", "Ma", "Romelia", "Aileen", "Son", "Kasie", "Donita", "Fumiko", "Emilie", "Violette", "Stacia", "Krysta", "Russel", "Rosaria", "Felicia", "Lulu", "Nyla", "Humberto", "Fabian", "Jordan", "Brittney", "Jesenia", "Robbi", "Erinn", "Brett", "Sharla", "Freida", "Letisha", "Toi", "Lorenza", "Gerda", "Glennie", "Geoffrey", "Cassy", "Janis", "Margy", "Esperanza", "Leonor", "Claudie", "Shaquana", "Harriett", "Ona", "Rikki", "Cherrie", "Eduardo", "Johanna", "Jamel" };
+        private static string[] lastNames = { "Cronkhite", "Gilcrease", "Easterling", "Kenner", "Culotta", "Yingling", "Dauenhauer", "Bohland", "Stansberry", "Hobgood", "Adair", "Finley", "Lebaron", "Steinberg", "Emmer", "Kehl", "Miland", "Guillermo", "Palmateer", "Romines", "Walko", "Tinkler", "Spurr", "Linz", "Latorre", "Kwiecien", "Olin", "Lodge", "Rogowski", "Mcmurtry", "Binns", "Brandstetter", "Ostby", "Sherburne", "Brewer", "Arboleda", "Ackman", "Ahlgren", "Rainwater", "Bushell", "Bitton", "Dauphin", "Short", "Benjamin", "Fergerson", "Aigner", "Kilman", "Esterly", "Bueche", "Dulle" };
+        private static string resPath = @"C:\Users\morit\Source\Repos\DC-Workshops\DatabaseLib\res\";
+        private static string[] imagePaths = { resPath + @"1.jpg", resPath + @"2.jpg", resPath + @"3.jpg", resPath + @"4.jpg", resPath + @"5.jpg", resPath + @"6.jpg", resPath + @"7.jpg", resPath + @"8.jpg", resPath + @"9.jpg", resPath + @"10.jpg" };
+
+        private Random m_rand;
+
+        public DatabaseGenerator()
+        {
+            Console.WriteLine("generating...");
+            m_rand = new Random();
+        }
+
+        public void GetNextAccount(out uint pin, out uint acctNo, out string firstName, out string lastName, out int balance, out string imagePath)
+        {
+            pin = GetPIN();
+            acctNo = GetAcctNo();
+            firstName = GetFirstname();
+            lastName = GetLastname();
+            balance = GetBalance();
+            imagePath = GetImagePath();
+        }
+
+        private string GetFirstname()
+        {
+            return firstNames[m_rand.Next(firstNames.Length)];
+        }
+        private string GetLastname()
+        {
+            return lastNames[m_rand.Next(lastNames.Length)];
+        }
+
+        private uint GetPIN()
+        {
+            return Convert.ToUInt32(m_rand.Next(999999));
+        }
+        private uint GetAcctNo()
+        {
+            return Convert.ToUInt32(m_rand.Next(999999));
+        }
+        private int GetBalance()
+        {
+            return m_rand.Next(999999);
+        }
+
+        private string GetImagePath()
+        {
+            return imagePaths[m_rand.Next(imagePaths.Length)];
+        }
+    }
+}
---
+++ w4/ServerView/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.ServiceModel;
+using ServerProg;
+using System;
+using ServerInterfaceLib;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+
+namespace ServerView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ServerInterfaceLib.DataServerInterface m_server;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Console.WriteLine("Making new TCP request...");
+
+            //Set server URL 
+            string url = "net.tcp://localhost:8100/DataService";
+            NetTcpBinding tcp = new NetTcpBinding();
+            //tcp.TransferMode = TransferMode.StreamedResponse; //FIXME for some reason this causes the constructor to be reloaded each time AND also causes fault contracts to not work jesus christ 🤔
+            tcp.MaxReceivedMessageSize = 1024 * 1024 * 2; //NOTE: Why 2?
+
+            //Create connection factory for connection to server
+            ChannelFactory<ServerInterfaceLib.DataServerInterface> serverChannelFactory = new ChannelFactory<DataServerInterface>(tcp, url);
+            m_server = serverChannelFactory.CreateChannel();
+
+            //Also, tell me how many entries are in the DB.
+            TotalEntriesBox.Text = m_server.GetNumEntries().ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 0;
+            string fName = "", lName = "";
+            int bal = 0;
+            uint acct = 0, pin = 0;
+
+            try
+            {
+                //On click, Get the index....
+                index = Int32.Parse(IndexEditBox.Text);
+
+                //Then, run our RPC function, using the out mode parameters...
+                m_server.GetValuesForEntry(index, out acct, out pin, out bal, out fName, out lName);
+
+                //And now, set the values in the GUI!
+                FirstNameBox.Text = fName;
+                LastNameBox.Text = lName;
+                BalanceBox.Text = bal.ToString("C");
+                AccountNumBox.Text = acct.ToString();
+                PinBox.Text = pin.ToString("D4");
+
+                //Display the profile image, getting it from the image stream
+                ImageBox.Source = BitmapFrame.Create(m_server.GetImageForEntry(index));
+            } catch (FormatException)
+            {
+                MessageBox.Show("Please insert only numbers into the index box", "Bad Input", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (FaultException<DatabaseAccessFault> d)
+            {
+                MessageBox.Show(d.Detail.Message, "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            } catch (FaultException f)
+            {
+                MessageBox.Show("An unknown error has occured.", "Unknown Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            } catch
+            {
+                MessageBox.Show("Beans.", "Unknown Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
