9377f6518f48b5432c61bc1ac2790d83335c9005
Moritz Bergemann <49220792+Moritz-Bergemann@users.noreply.github.com>
2021-05-06 22:22:30 +0800

    feat: Remove unecessary API components/pages

--- ThreeTierWebService/BusinessTier/Controllers/BankViewController.cs
+++ ThreeTierWebService/BusinessTier/Controllers/BankViewController.cs
@@ -11,47 +11,6 @@ namespace BusinessTier.Controllers
 {
     public class BankViewController : Controller
     {
-        private RestClient _businessClient = new RestClient("https://localhost:44347/");
-
-        //TODO remove all this
-        //Web utility access
-        private UserData GetUserData(int userId)
-        {
-            RestRequest request = new RestRequest("api/user/" + userId); 
-            IRestResponse response = _businessClient.Get(request);
-
-            UserData data = JsonConvert.DeserializeObject<UserData>(response.Content);
-
-            //TODO error handling (what to do here??)
-
-            return data;
-        }
-
-        private AccountData GetAccountData(int accountId)
-        {
-            RestRequest request = new RestRequest("api/account/" + accountId);
-            IRestResponse response = _businessClient.Get(request);
-            
-            AccountData data = JsonConvert.DeserializeObject<AccountData>(response.Content);
-
-            //TODO error handling (what to do here??)
-
-            return data;
-        }
-
-        private List<TransactionData> GetAccountTransactions(int accountId)
-        {
-            RestRequest request = new RestRequest($"api/account/{accountId}/transactions");
-            IRestResponse response = _businessClient.Get(request);
-
-            List<TransactionData> data = JsonConvert.DeserializeObject<List<TransactionData>>(response.Content);
-
-            //TODO error handling (what to do here??)
-
-            return data;
-
-        }
-
         [Route("")]
         public ActionResult Index()
         {
@@ -73,16 +32,6 @@ namespace BusinessTier.Controllers
         [Route("transaction")]
         public ActionResult TransactionPage()
         {
-            return View();
-        }
-
-
-        [Route("account/{accountId}")]
-        public ActionResult AccountPage(int accountId)
-        {
-            ViewBag.AccountData = GetAccountData(accountId);
-            ViewBag.Transactions = GetAccountTransactions(accountId);
-
             return View();
         }
     }
