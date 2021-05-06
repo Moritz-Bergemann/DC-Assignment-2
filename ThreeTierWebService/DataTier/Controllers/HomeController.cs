e6a4e886c06c93f18d4e6153bd517cbb6eff1eaf
Moritz Bergemann <49220792+Moritz-Bergemann@users.noreply.github.com>
2021-05-06 22:32:33 +0800

    feat: Remove unnecessary data tier controller methods

--- ThreeTierWebService/DataTier/Controllers/HomeController.cs
+++ ThreeTierWebService/DataTier/Controllers/HomeController.cs
@@ -12,25 +12,5 @@ namespace DataTier.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        // GET api/<controller>
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
     }
 }
