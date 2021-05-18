1f1e79ce371e61294e706f69a199e50917a276bb
Moritz Bergemann <49220792+Moritz-Bergemann@users.noreply.github.com>
2021-05-18 16:13:13 +0800

    feat: Add web "get num entries"

--- prac3/BusinessWebApp/Controllers/ValuesController.cs
+++ prac3/BusinessWebApp/Controllers/ValuesController.cs
@@ -9,41 +9,12 @@ using BusinessTier;
 
 namespace BusinessWebApp.Controllers
 {
-    //public class ValuesController : ApiController
-    //{
-    //    // GET api/<controller>
-    //    public string Get()
-    //    {
-    //        return BusinessModel.Instance.GetNumEntries().ToString();
-    //    }
-    //}
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public int Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return $"value {id}";
-        }
-
-        // POST api/values
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
+            return BusinessModel.Instance.GetNumEntries();
         }
     }
 }
